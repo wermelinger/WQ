8c5cd6829b44c4dc3705ed7c4ce64b9bc93dd0f1
Stefan Wermelinger <stefan.wermelinger@abix.ch>
2015-10-11 13:23:12 +0200

    refactoring of openweatherservice

--- app/common/openWeatherService.js
+++ app/common/openWeatherService.js
@@ -10,27 +10,11 @@ var app;
                 this.kelvinToCelsiusFactor = -272.15;
                 that = this;
             }
-            OpenWeatherService.prototype.getUser = function (locationName) {
-                var _this = this;
-                var location = new app.domain.Location();
-                this.$resource("https://api.github.com/users/" + locationName + "&APPID=" + this.apiKey).get(function (data) {
-                    location.login = data.login;
-                    location.location = data.location;
-                    location.avatar_url = data.avatar_url;
-                    location.repos = _this.getRepos(data.repos_url);
-                });
-                return location;
-            };
             OpenWeatherService.prototype.getCurrentWeather = function (locationName, onWeatherFetchedCallback) {
-                var _this = this;
                 var weather = new app.domain.CurrentWeather();
-                this.$resource("http://api.openweathermap.org/data/2.5/weather?q=" + locationName).get(function (data) {
+                this.$resource("http://api.openweathermap.org/data/2.5/weather?q=" + locationName + "&APPID=" + this.apiKey).get(function (data) {
                     // Update Weather data
-                    weather.name = data.name;
-                    weather.flagimage = data.sys.country.toLowerCase();
-                    weather.longitude = data.coord.lon;
-                    weather.latitude = data.coord.lat;
-                    weather.weather = new app.domain.Weather(data.main.temp + _this.kelvinToCelsiusFactor, data.main.pressure, data.main.humidity, data.main.temp_min + _this.kelvinToCelsiusFactor, data.main.temp_max + _this.kelvinToCelsiusFactor, data.weather[0].icon);
+                    that.fillWeatherData(weather, data);
                     that.fillWeatherNearby(weather);
                     // Notify callback
                     onWeatherFetchedCallback(weather);
@@ -38,34 +22,21 @@ var app;
                 return weather;
             };
             OpenWeatherService.prototype.fillWeatherNearby = function (weather) {
-                that.$resource("http://api.openweathermap.org/data/2.5/find?lat=" + weather.latitude + "&lon=" + weather.longitude + "&cnt=10").get(function (data) {
+                that.$resource("http://api.openweathermap.org/data/2.5/find?lat=" + weather.latitude + "&lon=" + weather.longitude + "&cnt=10" + "&APPID=" + this.apiKey).get(function (data) {
                     // Update Weather data
                     data.list.forEach(function (element) {
-                        var weatherObj = that.createWeatherData(element);
+                        var weatherObj = new app.domain.CurrentWeather();
+                        that.fillWeatherData(weatherObj, element);
                         weather.nearbyWeather.push(weatherObj);
                     });
                 });
             };
-            OpenWeatherService.prototype.createWeatherData = function (data) {
-                var weather = new app.domain.CurrentWeather();
+            OpenWeatherService.prototype.fillWeatherData = function (weather, data) {
                 weather.name = data.name;
                 weather.flagimage = data.sys.country.toLowerCase();
                 weather.longitude = data.coord.lon;
                 weather.latitude = data.coord.lat;
                 weather.weather = new app.domain.Weather(data.main.temp + this.kelvinToCelsiusFactor, data.main.pressure, data.main.humidity, data.main.temp_min + this.kelvinToCelsiusFactor, data.main.temp_max + this.kelvinToCelsiusFactor, data.weather[0].icon);
-                return weather;
-            };
-            OpenWeatherService.prototype.getRepos = function (url) {
-                var repos = new Array();
-                this.$resource(url).query(function (data) {
-                    data.forEach(function (element) {
-                        var repo = new app.domain.Repo();
-                        repos.push(repo);
-                        repo.name = element.name;
-                        repo.description = element.description;
-                    });
-                });
-                return repos;
             };
             OpenWeatherService.$inject = ["$resource"];
             return OpenWeatherService;
--- app/common/openWeatherService.ts
+++ app/common/openWeatherService.ts
@@ -1,6 +1,5 @@
 module app.common {
 	interface IOpenWeatherService {
-		getUser(userName : string): app.domain.Location;
 		getCurrentWeather(locationName: string, onWeatherFetchedCallback : (weather : app.domain.CurrentWeather) => void) : app.domain.CurrentWeather;
 	}
 
@@ -15,58 +14,32 @@ module app.common {
 			that = this;
 		}
 		
-		getUser(locationName : string): app.domain.Location {
-			var location = new app.domain.Location();
-			this.$resource("https://api.github.com/users/" + locationName + "&APPID=" + this.apiKey).get(data => {
-					location.login = data.login;
-					location.location = data.location;
-					location.avatar_url = data.avatar_url;
-					location.repos = this.getRepos(data.repos_url);
-				});
-				
-			return location;
-		}
-		
 		getCurrentWeather(locationName: string, onWeatherFetchedCallback : (weather : app.domain.CurrentWeather) => void) : app.domain.CurrentWeather {
 			var weather = new app.domain.CurrentWeather();
-			this.$resource("http://api.openweathermap.org/data/2.5/weather?q=" + locationName).get(data => {
+			this.$resource("http://api.openweathermap.org/data/2.5/weather?q=" + locationName + "&APPID=" + this.apiKey).get(data => {
 					// Update Weather data
-					weather.name = data.name;
-					weather.flagimage = data.sys.country.toLowerCase();
-					weather.longitude = data.coord.lon;
-					weather.latitude = data.coord.lat;
-					weather.weather = new app.domain.Weather(
-						data.main.temp + this.kelvinToCelsiusFactor, 
-						data.main.pressure, 
-						data.main.humidity, 
-						data.main.temp_min + this.kelvinToCelsiusFactor, 
-						data.main.temp_max + this.kelvinToCelsiusFactor,
-						data.weather[0].icon);
-						
+					that.fillWeatherData(weather, data);	
 					that.fillWeatherNearby(weather);
 
 					// Notify callback
 					onWeatherFetchedCallback(weather);
 				});
-			
-			
+
 			return weather;
 		}
 		
 		private fillWeatherNearby(weather : app.domain.CurrentWeather) {
-			that.$resource("http://api.openweathermap.org/data/2.5/find?lat=" + weather.latitude + "&lon=" + weather.longitude + "&cnt=10").get(data => {
+			that.$resource("http://api.openweathermap.org/data/2.5/find?lat=" + weather.latitude + "&lon=" + weather.longitude + "&cnt=10" + "&APPID=" + this.apiKey).get(data => {
 					// Update Weather data
 					data.list.forEach(element => {
-						var weatherObj = that.createWeatherData(element);
+						var weatherObj = new app.domain.CurrentWeather();
+						that.fillWeatherData(weatherObj, element);
 						weather.nearbyWeather.push(weatherObj); 
 					});
-					
-					
 				});
 		}
 		
-		private createWeatherData(data : any) : app.domain.CurrentWeather  {
-			var weather = new app.domain.CurrentWeather();
+		private fillWeatherData(weather : app.domain.CurrentWeather, data : any) {
 			weather.name = data.name;
 			weather.flagimage = data.sys.country.toLowerCase();
 			weather.longitude = data.coord.lon;
@@ -78,22 +51,6 @@ module app.common {
 				data.main.temp_min + this.kelvinToCelsiusFactor, 
 				data.main.temp_max + this.kelvinToCelsiusFactor,
 				data.weather[0].icon);
-				
-			return weather;
-		}
-		
-		getRepos(url : string): Array<app.domain.Repo> {
-			var repos = new Array<app.domain.Repo>();
-			this.$resource(url).query(data => {
-					data.forEach(element => {
-						var repo = new app.domain.Repo();
-						repos.push(repo);
-						repo.name = element.name;
-						repo.description = element.description;
-					});
-				});
-			
-			return repos; 
 		}
 	}
 	
